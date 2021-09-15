4927cc1662d7dd1294962da4b5d3cfb3ac408f2d
Benton Tripp <bentontripp@gmail.com>
2021-09-15 12:49:02 -0400

    commit 9/15/2021 12:48 PM

--- code/bingo/one_winner_bingo/static/JS/scripts.js
+++ code/bingo/one_winner_bingo/static/JS/scripts.js
@@ -22,7 +22,7 @@ function addField()
 function getCol()
 {
     var table = document.getElementById('tbl');
-    var totalCards = document.getElementById('total_cards').rows[1].cells[1].childNodes[0].value
+    var totalCards = document.getElementById('total_cards1').rows[1].cells[1].childNodes[0].value
     var column = [];
     column.push(totalCards)
     for(var i = 0; i<table.rows.length; i++){
@@ -103,5 +103,67 @@ function removeField()
 
 function getCol_classic()
 {
-  alert('Classic');
+  var totalCards = document.getElementById('total_cards2').rows[1].cells[1].childNodes[0].value;
+  const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+    $.ajax({
+        headers: {'X-CSRFToken': csrftoken},
+        url: 'get_classic_bingo',
+        type: 'POST',
+        data: {'cards':totalCards},
+        success:function (response) {
+          if (response=='Error Code 1') {
+            var modal1 = document.getElementById("error1-Modal");
+            var span1 = document.getElementsByClassName("close")[0];
+            modal1.style.display = "block";
+            span1.onclick = function() {
+              modal1.style.display = "none";
+            }
+            window.onclick = function(event) {
+              if (event.target == modal1) {
+                modal1.style.display = "none";
+              }
+            }
+          }
+          else if (response=='Error Code 2') {
+            var modal2 = document.getElementById("error2-Modal");
+            var span2 = document.getElementsByClassName("close")[1];
+            modal2.style.display = "block";
+            span2.onclick = function() {
+              modal2.style.display = "none";
+            }
+            window.onclick = function(event) {
+              if (event.target == modal2) {
+                modal2.style.display = "none";
+              }
+            }
+          }
+          else if (response=='Error Code 3') {
+            var modal3 = document.getElementById("error3-Modal");
+            var span3 = document.getElementsByClassName("close")[2];
+            modal3.style.display = "block";
+            span3.onclick = function() {
+              modal3.style.display = "none";
+            }
+            window.onclick = function(event) {
+              if (event.target == modal3) {
+                modal3.style.display = "none";
+              }
+            }
+          }
+          else {download(response, 'BINGO_CARDS.pdf', 'application/pdf' );
+        }},
+        error:function () {
+          var modal4 = document.getElementById("error3-Modal");
+            var span4 = document.getElementsByClassName("close")[0];
+            modal4.style.display = "block";
+            span4.onclick = function() {
+              modal4.style.display = "none";
+            }
+            window.onclick = function(event) {
+              if (event.target == modal4) {
+                modal4.style.display = "none";
+              }
+            }
+          }
+      });
 }
--- code/bingo/one_winner_bingo/urls.py
+++ code/bingo/one_winner_bingo/urls.py
@@ -5,7 +5,6 @@ app_name = 'one_winner_bingo'
 
 urlpatterns = [
     path('', views.bingo, name = 'bingo'),
-    #path('pages/original_bingo.html', views.original_bingo),
-    #path('pages/custom_bingo.html', views.custom_bingo),
-    path('get_custom_bingo', views.get_custom_bingo, name='get_custom_bingo')
+    path('get_custom_bingo', views.get_custom_bingo, name='get_custom_bingo'),
+    path('get_classic_bingo', views.get_classic_bingo, name='get_classic_bingo')
 ]
